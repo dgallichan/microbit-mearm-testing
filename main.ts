3a6d6de1b444b1730f50d342964bbd479d3814db
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-16 17:25:35 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    mearm.moveByAngle(MearmServo.Base, 10)
+    servoGrip += 10
 })
 input.onButtonPressed(Button.B, function () {
-    mearm.moveByAngle(MearmServo.Base, -10)
+    servoGrip += -10
 })
+radio.onReceivedValue(function (name, value) {
+	
+})
+radio.setGroup(252)
 basic.showLeds(`
     . # # # .
     # . . . #
@@ -11,6 +15,14 @@ basic.showLeds(`
     # # . # #
     # . . . #
     `)
+let servoLeft = 0
+let servoRight = 0
+let servoGrip = 0
+let servoBase = 0
 basic.forever(function () {
-	
+    mearm.moveToAngle(MearmServo.Base, servoBase)
+    mearm.moveToAngle(MearmServo.Right, servoRight)
+    mearm.moveToAngle(MearmServo.Left, servoLeft)
+    mearm.moveToAngle(MearmServo.Grip, servoGrip)
+    basic.pause(100)
 })
